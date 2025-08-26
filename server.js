9a81eab3f4d20926ae5b8c5be4ef82e28390971c
hichambenguesmia00-byte <hichambenguesmia00@gmail.com>
2025-08-26 19:31:21 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,24 +2,24 @@ const express = require('express');
 const multer = require('multer');
 const fs = require('fs');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-// إعداد Multer
+// تفعيل CORS
+app.use(cors());
+
 const upload = multer({ dest: 'uploads/' });
 
-// Route اختبار السيرفر
 app.get('/', (req, res) => {
   res.send('✅ QuickTools Backend Running!');
 });
 
-// رفع ملف وإرجاعه مباشرة
 app.post('/upload', upload.single('file'), (req, res) => {
   if(!req.file) return res.status(400).send('No file uploaded');
 
   const outputPath = path.join(__dirname, req.file.originalname);
-
   fs.renameSync(req.file.path, outputPath);
 
   res.download(outputPath, req.file.originalname, () => {
